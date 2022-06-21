55009ae75620b15c4d1a792e178a39debfe2a6f5
Fevzi Can Şahinler <84748585+fevzisahinler@users.noreply.github.com>
2022-06-21 16:29:58 +0300

    Update birthdayCakeCandles.java

--- birthdayCakeCandles.java
+++ birthdayCakeCandles.java
@@ -1,18 +1,27 @@
-import java.util.List;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class birthdayCakeCandles {
+class Result {
 
-    public static int birthdayCakeCandles(List<Integer> candles) {  
+    public static int birthdayCakeCandles(List<Integer> candles) {
         int maxnumber= 1;
         int count = 0;
-        
-        // ilk for döngüsü ile maximum uzunluktaki mumu bulduk 
-        for(int i = 0 ; i<candles.size()-1 ; i++){ 
-            if(candles.get(i)>maxnumber){ 
+        // ilk for döngüsü ile maximum uzunluktaki mumu buluyoruz
+        for(int i = 0 ; i<candles.size()-1 ; i++){
+            if(candles.get(i)>maxnumber){
                 maxnumber = candles.get(i);
             }
         }
-        // bu for döngüsü ile bulduğumuz maximum uzunluktaki mumları saydık 
+        // ikinci for döngüsü ile maximum uzunluktaki mumun kaç adet olduğunu buluyoruz
         for(int j : candles){
             if( j == maxnumber){
                 count +=1;
@@ -20,5 +29,27 @@ public class birthdayCakeCandles {
                 
         }
         return count;
+    }
+    
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
